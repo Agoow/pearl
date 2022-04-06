a49a273ecdabbccb5d243382cd740582d6a717cd
Agow <46010145+Agoow@users.noreply.github.com>
2022-04-06 21:39:28 +0200

    Update reactions.py

--- addons/reactions.py
+++ addons/reactions.py
@@ -81,7 +81,7 @@ class reactions(commands.Cog):
         if msg.startswith('!p'):
             await message.delete()
 
-        if word in Swords:
+        if "lowest price" in msg.content:
            #print(type(message))
             #print(msg.content)
             #await asyncio.sleep(5)
